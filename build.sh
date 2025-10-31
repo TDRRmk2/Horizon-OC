64df7694b94ed03a2eb6d7177613b36d3c7b2f44
souldbminersmwc <souldbyt@gmail.com>
2025-10-31 19:37:02 -0400

    hoc-monitor: add hoc monitor

--- build.sh
+++ build.sh
@@ -10,13 +10,18 @@ cp -r "$SRC"/. "$DEST"/
 cd build/stratosphere/loader || exit 1
 make -j"$(nproc)"
 hactool -t kip1 out/nintendo_nx_arm64_armv8a/release/loader.kip --uncompress=hoc.kip
-rm out/nintendo_nx_arm64_armv8a/release/loader.kip
 cd ../../../ # exit
 cp build/stratosphere/loader/hoc.kip dist/atmosphere/kips/hoc.kip
 
 cd Source/sys-clk/
 ./build.sh
 cp -r dist/ ../../
+
+cd ../../
+
+cd Source/hoc-monitor/
+make -j"$(nproc)"
+cp hoc-monitor.ovl ../../dist/switch/.overlays/hoc-monitor.ovl
 
 # cd ../../Source/configurator
 
